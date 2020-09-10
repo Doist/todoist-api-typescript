ee053f7a98b661f44e527e0f9b27756ccb9822b6
David Fishlock <email@davidfishlock.com>
2020-09-10 15:59:37 +0100

    Add task update/close/reopen/delete methods. Refactor restclient. Add missing response verification tests for some endpoints.

--- src/TodoistApi.test.ts
+++ src/TodoistApi.test.ts
@@ -8,19 +8,18 @@ import { DEFAULT_PROJECT, DEFAULT_QUICK_ADD_RESPONSE, DEFAULT_TASK } from './tes
 import {
     API_REST_BASE_URI,
     API_SYNC_BASE_URI,
+    ENDPOINT_REST_TASK_CLOSE,
     ENDPOINT_REST_PROJECTS,
+    ENDPOINT_REST_TASK_REOPEN,
     ENDPOINT_REST_TASKS,
     ENDPOINT_SYNC_QUICK_ADD,
 } from './consts/endpoints'
 
 const DEFAULT_AUTH_TOKEN = 'AToken'
 
-const setupRestClientMock = (responseData: unknown) => {
-    const response = mock<AxiosResponse>({ data: responseData })
-    return {
-        getMock: jest.spyOn(restClient, 'get').mockResolvedValue(response),
-        postMock: jest.spyOn(restClient, 'post').mockResolvedValue(response),
-    }
+const setupRestClientMock = (responseData: unknown, status = 200) => {
+    const response = mock<AxiosResponse>({ status, data: responseData })
+    return jest.spyOn(restClient, 'request').mockResolvedValue(response)
 }
 
 const setupSyncTaskConverter = (returnedTask: Task) => {
@@ -38,12 +37,14 @@ describe('TodoistApi', () => {
         }
 
         test('calls post on restClient with expected parameters', async () => {
-            const { postMock } = setupRestClientMock(DEFAULT_TASK)
+            const requestMock = setupRestClientMock(DEFAULT_TASK)
             const api = getTarget()
+
             await api.addTask(DEFAULT_ADD_TASK_ARGS)
 
-            expect(postMock).toBeCalledTimes(1)
-            expect(postMock).toBeCalledWith(
+            expect(requestMock).toBeCalledTimes(1)
+            expect(requestMock).toBeCalledWith(
+                'POST',
                 API_REST_BASE_URI,
                 ENDPOINT_REST_TASKS,
                 DEFAULT_AUTH_TOKEN,
@@ -51,13 +52,123 @@ describe('TodoistApi', () => {
             )
         })
 
-        test('returns data from restClient post response', async () => {
+        test('returns result from rest client', async () => {
             setupRestClientMock(DEFAULT_TASK)
-
             const api = getTarget()
+
             const task = await api.addTask(DEFAULT_ADD_TASK_ARGS)
 
             expect(task).toEqual(DEFAULT_TASK)
+        })
+    })
+
+    describe('updateTask', () => {
+        test('calls post on restClient with expected parameters', async () => {
+            const taskId = 123
+            const updateArgs = { content: 'some new content' }
+            const requestMock = setupRestClientMock(true)
+            const api = getTarget()
+
+            await api.updateTask(taskId, updateArgs)
+
+            expect(requestMock).toBeCalledTimes(1)
+            expect(requestMock).toBeCalledWith(
+                'POST',
+                API_REST_BASE_URI,
+                `${ENDPOINT_REST_TASKS}/${taskId}`,
+                DEFAULT_AUTH_TOKEN,
+                updateArgs,
+            )
+        })
+
+        test('returns result from rest client', async () => {
+            setupRestClientMock(true)
+            const api = getTarget()
+
+            const response = await api.updateTask(123, { content: 'some content' })
+
+            expect(response).toEqual(true)
+        })
+    })
+
+    describe('closeTask', () => {
+        test('calls post on close endpoint', async () => {
+            const taskId = 123
+            const requestMock = setupRestClientMock(true)
+            const api = getTarget()
+
+            await api.closeTask(taskId)
+
+            expect(requestMock).toBeCalledTimes(1)
+            expect(requestMock).toBeCalledWith(
+                'POST',
+                API_REST_BASE_URI,
+                `${ENDPOINT_REST_TASKS}/${taskId}/${ENDPOINT_REST_TASK_CLOSE}`,
+                DEFAULT_AUTH_TOKEN,
+            )
+        })
+
+        test('returns result from rest client', async () => {
+            setupRestClientMock(true)
+            const api = getTarget()
+
+            const response = await api.closeTask(123)
+
+            expect(response).toEqual(true)
+        })
+    })
+
+    describe('reopenTask', () => {
+        test('calls post on reopen endpoint', async () => {
+            const taskId = 123
+            const requestMock = setupRestClientMock(true)
+            const api = getTarget()
+
+            await api.reopenTask(taskId)
+
+            expect(requestMock).toBeCalledTimes(1)
+            expect(requestMock).toBeCalledWith(
+                'POST',
+                API_REST_BASE_URI,
+                `${ENDPOINT_REST_TASKS}/${taskId}/${ENDPOINT_REST_TASK_REOPEN}`,
+                DEFAULT_AUTH_TOKEN,
+            )
+        })
+
+        test('returns result from rest client', async () => {
+            setupRestClientMock(true)
+            const api = getTarget()
+
+            const response = await api.reopenTask(123)
+
+            expect(response).toEqual(true)
+        })
+    })
+
+    describe('deleteTask', () => {
+        test('calls delete on expected task', async () => {
+            const taskId = 123
+            const requestMock = setupRestClientMock(true)
+            const api = getTarget()
+
+            await api.deleteTask(taskId)
+
+            expect(requestMock).toBeCalledTimes(1)
+            expect(requestMock).toBeCalledWith(
+                'DELETE',
+                API_REST_BASE_URI,
+                `${ENDPOINT_REST_TASKS}/${taskId}`,
+                DEFAULT_AUTH_TOKEN,
+            )
+        })
+
+        test('returns result from rest client', async () => {
+            setupRestClientMock(true)
+            const api = getTarget()
+
+            const response = await api.deleteTask(123)
+
+            expect(response).toEqual(true)
         })
     })
 
@@ -65,12 +176,14 @@ describe('TodoistApi', () => {
         const DEFAULT_QUICK_ADD_TEXT = 'This is a quick add text'
 
         test('calls sync endpoint with expected parameters', async () => {
-            const { postMock } = setupRestClientMock(DEFAULT_QUICK_ADD_RESPONSE)
+            const requestMock = setupRestClientMock(DEFAULT_QUICK_ADD_RESPONSE)
             const api = getTarget()
+
             await api.quickAddTask(DEFAULT_QUICK_ADD_TEXT)
 
-            expect(postMock).toBeCalledTimes(1)
-            expect(postMock).toBeCalledWith(
+            expect(requestMock).toBeCalledTimes(1)
+            expect(requestMock).toBeCalledWith(
+                'POST',
                 API_SYNC_BASE_URI,
                 ENDPOINT_SYNC_QUICK_ADD,
                 DEFAULT_AUTH_TOKEN,
@@ -81,8 +194,8 @@ describe('TodoistApi', () => {
         test('calls task converter with response data and returns result', async () => {
             setupRestClientMock(DEFAULT_QUICK_ADD_RESPONSE)
             const taskConverter = setupSyncTaskConverter(DEFAULT_TASK)
-
             const api = getTarget()
+
             const task = await api.quickAddTask(DEFAULT_QUICK_ADD_TEXT)
 
             expect(taskConverter).toBeCalledTimes(1)
@@ -94,12 +207,14 @@ describe('TodoistApi', () => {
     describe('getTask', () => {
         test('calls get request with expected url', async () => {
             const taskId = 12
-            const { getMock } = setupRestClientMock(DEFAULT_TASK)
+            const requestMock = setupRestClientMock(DEFAULT_TASK)
             const api = getTarget()
+
             await api.getTask(taskId)
 
-            expect(getMock).toBeCalledTimes(1)
-            expect(getMock).toBeCalledWith(
+            expect(requestMock).toBeCalledTimes(1)
+            expect(requestMock).toBeCalledWith(
+                'GET',
                 API_REST_BASE_URI,
                 `${ENDPOINT_REST_TASKS}/${taskId}`,
                 DEFAULT_AUTH_TOKEN,
@@ -113,48 +228,83 @@ describe('TodoistApi', () => {
         }
 
         test('calls get on expected endpoint with args', async () => {
-            const { getMock } = setupRestClientMock([DEFAULT_TASK])
+            const requestMock = setupRestClientMock([DEFAULT_TASK])
             const api = getTarget()
+
             await api.getTasks(DEFAULT_GET_TASKS_ARGS)
 
-            expect(getMock).toBeCalledTimes(1)
-            expect(getMock).toBeCalledWith(
+            expect(requestMock).toBeCalledTimes(1)
+            expect(requestMock).toBeCalledWith(
+                'GET',
                 API_REST_BASE_URI,
                 ENDPOINT_REST_TASKS,
                 DEFAULT_AUTH_TOKEN,
                 DEFAULT_GET_TASKS_ARGS,
             )
         })
+
+        test('returns result from rest client', async () => {
+            const tasks = [DEFAULT_TASK]
+            setupRestClientMock(tasks)
+            const api = getTarget()
+
+            const response = await api.getTasks(DEFAULT_GET_TASKS_ARGS)
+
+            expect(response).toEqual(tasks)
+        })
     })
 
     describe('getProject', () => {
         test('calls get request with expected url', async () => {
             const projectId = 12
-            const { getMock } = setupRestClientMock(DEFAULT_PROJECT)
+            const requestMock = setupRestClientMock(DEFAULT_PROJECT)
             const api = getTarget()
+
             await api.getProject(projectId)
 
-            expect(getMock).toBeCalledTimes(1)
-            expect(getMock).toBeCalledWith(
+            expect(requestMock).toBeCalledTimes(1)
+            expect(requestMock).toBeCalledWith(
+                'GET',
                 API_REST_BASE_URI,
                 `${ENDPOINT_REST_PROJECTS}/${projectId}`,
                 DEFAULT_AUTH_TOKEN,
             )
         })
+
+        test('returns result from rest client', async () => {
+            setupRestClientMock(DEFAULT_PROJECT)
+            const api = getTarget()
+
+            const project = await api.getProject(123)
+
+            expect(project).toEqual(DEFAULT_PROJECT)
+        })
     })
 
     describe('getProjects', () => {
         test('calls get on projects endpoint', async () => {
-            const { getMock } = setupRestClientMock([DEFAULT_PROJECT])
+            const requestMock = setupRestClientMock([DEFAULT_PROJECT])
             const api = getTarget()
+
             await api.getProjects()
 
-            expect(getMock).toBeCalledTimes(1)
-            expect(getMock).toBeCalledWith(
+            expect(requestMock).toBeCalledTimes(1)
+            expect(requestMock).toBeCalledWith(
+                'GET',
                 API_REST_BASE_URI,
                 ENDPOINT_REST_PROJECTS,
                 DEFAULT_AUTH_TOKEN,
             )
+        })
+
+        test('returns result from rest client', async () => {
+            const projects = [DEFAULT_PROJECT]
+            setupRestClientMock(projects)
+            const api = getTarget()
+
+            const response = await api.getProjects()
+
+            expect(response).toEqual(projects)
         })
     })
 })
---
+++ src/types/http.ts
@@ -0,0 +1,1 @@
+export type HttpMethod = 'POST' | 'GET' | 'DELETE'
