1211ded88bf077d2b17087e21d17072dd0b40781
Pedro Alves <pedroalves0@gmail.com>
2025-01-21 10:25:31 +0100

    chore: bump version to 4.0.0-alpha.1

--- jest.config.js
+++ jest.config.js
@@ -1,7 +1,7 @@
 module.exports = {
-    preset: "ts-jest",
+    preset: 'ts-jest',
     transform: { '^.+\\.ts': 'ts-jest' },
     testMatch: ['**/*.test.ts'],
     clearMocks: true,
-    testEnvironment: 'node'
+    testEnvironment: 'node',
 }
