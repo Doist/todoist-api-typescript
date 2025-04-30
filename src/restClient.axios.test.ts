2c8ecc73153268a324f8f0bc05d4c304ab34121b
Scott Lovegrove <scott@ferretlabs.com>
2025-04-30 10:44:45 +0100

    chore: Do not serialise null/undefined values for GET calls (#294)

--- src/restClient.axios.test.ts
+++ src/restClient.axios.test.ts
@@ -17,4 +17,32 @@ describe('axios tests without mocking', () => {
         })
         expect(requestUri).toEqual('https://api.todoist.com/rest/v2/tasks?ids=12345%2C56789')
     })
+
+    test('GET calls do not serialise null values', () => {
+        const requestUri = axios.create().getUri({
+            method: 'GET',
+            baseURL: DEFAULT_BASE_URI,
+            params: {
+                ids: null,
+            },
+            paramsSerializer: {
+                serialize: paramsSerializer,
+            },
+        })
+        expect(requestUri).toEqual('https://api.todoist.com/rest/v2/tasks')
+    })
+
+    test('GET calls do not serialise undefined values', () => {
+        const requestUri = axios.create().getUri({
+            method: 'GET',
+            baseURL: DEFAULT_BASE_URI,
+            params: {
+                ids: undefined,
+            },
+            paramsSerializer: {
+                serialize: paramsSerializer,
+            },
+        })
+        expect(requestUri).toEqual('https://api.todoist.com/rest/v2/tasks')
+    })
 })
